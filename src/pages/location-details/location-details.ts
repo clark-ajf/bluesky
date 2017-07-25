5fe781457eb75863bb87d8dc393e62e7a7ec699c
Clark Jeria <clark.ajf@gmail.com>
2017-07-25 10:58:26 -0700

    hints

--- src/pages/location-details/location-details.ts
+++ src/pages/location-details/location-details.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController, NavParams } from 'ionic-angular';
+import { NavController, ModalController, NavParams, ToastController } from 'ionic-angular';
 
 import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 
@@ -12,26 +12,38 @@ import { Location } from '../../models/location';
 export class LocationDetailsPage {
 
   location: Location;
-  scanData : {};
   options :BarcodeScannerOptions;
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public navParams: NavParams, private barcodeScanner: BarcodeScanner) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public navParams: NavParams, private barcodeScanner: BarcodeScanner, public toastCtrl: ToastController) {
     this.location = <Location> navParams.get('location');
   }
 
   ionViewDidLoad() {
   }
 
-  scan(){
+    scan(){
         this.options = {
             prompt : "Scan QR Code "
         }
         this.barcodeScanner.scan(this.options).then((barcodeData) => {
-
             console.log(barcodeData);
-            this.scanData = barcodeData;
         }, (err) => {
             console.log("Error occured : " + err);
+            const toast = this.toastCtrl.create({
+                message: 'Invalid QR code',
+                showCloseButton: true,
+                closeButtonText: 'Ok'
+            });
+            toast.present();
         });   
+    }
+
+    showHint(hint){
+        const toast = this.toastCtrl.create({
+            message: hint.message,
+            showCloseButton: true,
+            closeButtonText: 'Ok'
+        });
+        toast.present();
     }
 }
