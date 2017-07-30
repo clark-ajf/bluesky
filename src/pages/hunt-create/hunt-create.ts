1650bb688f0481156aa103e291323428cd5cb43b
Clark Jeria <clark.ajf@gmail.com>
2017-07-30 11:22:01 -0700

    camera component

--- src/pages/hunt-create/hunt-create.ts
+++ src/pages/hunt-create/hunt-create.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, ModalController, NavParams } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 import { LocationListPage } from '../location-list/location-list';
 
@@ -12,32 +13,53 @@ import { Hunt } from '../../models/hunt';
 })
 export class HuntCreatePage {
 
-  hunt: Hunt;
+  private hunt: Hunt;
+  private huntForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera, private formBuilder: FormBuilder) {
+      this.huntForm = this.formBuilder.group({
+          name: ['', Validators.compose([Validators.required])],
+          short_description: ['', Validators.compose([Validators.required])],
+          long_description: ['', Validators.compose([Validators.required])]
+      });
+
+      this.hunt = { img: '', message: '', name: '', time: ''}
   }
 
   ionViewDidLoad() {
   }
 
   next(){
-    //this.navCtrl.push(LocationListPage);
+    if(this.huntForm.valid && this.hunt.img != ''){
+        let form = this.huntForm.value;
+        this.hunt.name = form.name;
+        this.hunt.message = form.short_description;
+        //this.navCtrl.push(AddLocationsPage, {hunt: this.hunt});
+    }
   }
 
-  selectPicture(){
+  deleteImage(){
+    this.hunt.img = '';
+  }
+
+  selectPicture(takePicture: boolean){
     const options: CameraOptions = {
         quality: 100,
         destinationType: this.camera.DestinationType.DATA_URL,
         encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE
-        }
+        mediaType: this.camera.MediaType.PICTURE,
+        sourceType: takePicture? this.camera.PictureSourceType.CAMERA : this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+        targetHeight: 400,
+        targetWidth: 700
+    }    
 
-        this.camera.getPicture(options).then((imageData) => {
+    this.camera.getPicture(options).then((imageData) => {
         // imageData is either a base64 encoded string or a file URI
         // If it's base64:
         let base64Image = 'data:image/jpeg;base64,' + imageData;
-        }, (err) => {
+        this.hunt.img = base64Image;
+    }, (err) => {
         // Handle error
-        });
-    }
+    });
+  }
 }
