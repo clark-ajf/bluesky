038cf0eb61de9d8ce13493c207735d4f74724851
Clark Jeria <clark.ajf@gmail.com>
2017-07-29 19:12:25 -0700

    camera update

--- src/pages/hunt-create/hunt-create.ts
+++ src/pages/hunt-create/hunt-create.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, ModalController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import { LocationListPage } from '../location-list/location-list';
 
@@ -13,7 +14,7 @@ export class HuntCreatePage {
 
   hunt: Hunt;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -22,4 +23,21 @@ export class HuntCreatePage {
   next(){
     //this.navCtrl.push(LocationListPage);
   }
+
+  selectPicture(){
+    const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+        }
+
+        this.camera.getPicture(options).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        let base64Image = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+        // Handle error
+        });
+    }
 }
