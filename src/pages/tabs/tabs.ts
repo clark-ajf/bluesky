b775166cb03e3ad12129573b53d8757f99ccf411
Clark Jeria <clark.ajf@gmail.com>
2017-07-29 18:51:34 -0700

    fix login adding camera component

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -6,6 +6,13 @@ import { MyHuntsPage } from '../my-hunts/my-hunts';
 import { SearchPage } from '../search/search';
 import { AccountPage } from '../account/account';
 
+import { GooglePlus } from '@ionic-native/google-plus';
+
+import { WelcomePage } from '../welcome/welcome';
+
+import { SessionData } from '../../providers/session.data';
+import { User } from '../../models/user';
+
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html'
@@ -19,11 +26,23 @@ export class TabsPage {
   tab2Title = "Search";
   tab3Title = "Account";
 
-  constructor(public navCtrl: NavController, public translateService: TranslateService) {
+  constructor(public navCtrl: NavController, public translateService: TranslateService, private sessionData: SessionData, private googlePlus: GooglePlus) {
+    this.sessionData.hasLoggedIn().then((hasLoggedIn: boolean) => {
+      if (hasLoggedIn !== true) {
+        this.logout();
+      }
+    });
+
     translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE']).subscribe(values => {
       this.tab1Title = values['TAB1_TITLE'];
       this.tab2Title = values['TAB2_TITLE'];
       this.tab3Title = values['TAB3_TITLE'];
     });
+  }
+
+  logout() {
+    this.sessionData.logout();
+    this.googlePlus.logout();
+    this.navCtrl.setRoot(WelcomePage);
   }
 }
