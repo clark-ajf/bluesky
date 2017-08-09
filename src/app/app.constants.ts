cc4356cff2b797854664a9c4dc7cd19bbe630285
Clark <clark.ajf@gmail.com>
2017-08-08 20:19:40 -0700

    Update app.constants.ts
    server ip changes

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server: string = "http://35.185.219.43:9000/";
+    public Server: string = "http://35.197.126.9:9000/";
     public ApiUrl: string = "api/";
     public ServerWithApiUrl = this.Server + this.ApiUrl;
 
